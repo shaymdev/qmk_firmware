0bb0d07dc5cfc6760142b94b6c6c21bf1aee2343
Shay Merrill <shaymdev@gmail.com>
2019-01-31 16:51:24 -0700

    initial thumb cluster

--- keyboards/shergo/case/plate.clj
+++
@@ -1,5 +0,0 @@
-(ns shergo.case.plate
- (:refer-clojure :exclude [use import])
- (:require [scad-clj.scad :refer :all]
-  [scad-clj.model :refer :all]
-  [unicode-math.core :refer :all]))
--- keyboards/shergo/case/src/case/plate.clj
+++ keyboards/shergo/case/src/case/plate.clj
@@ -22,10 +22,10 @@
 ;---------------------------------------------------
 
 (def single-hole
-  (cube keyswitch-height keyswitch-width hole-thickness))
+  (cube keyswitch-width keyswitch-height hole-thickness))
 
 (def single-solid-plate
-  (cube (+ choc-cap-height space-between-caps) (+ choc-cap-width space-between-caps) plate-thickness))
+  (cube (+ choc-cap-width space-between-caps) (+ choc-cap-height space-between-caps) plate-thickness))
 
 (def single-hole-spaced
   (difference single-solid-plate
@@ -33,7 +33,7 @@
 
 (defn duplicate-in-column
   [thing key-index mapval]
-  (translate [(* key-index (+ choc-cap-height space-between-caps)) 0 0] thing))
+  (translate [0 (* key-index (+ choc-cap-height space-between-caps)) 0] thing))
 
 (defn column-plate
   [key-count]
@@ -41,33 +41,62 @@
 
 (defn translate-thing-by-offset
   [thing column-index offset]
-  (translate [offset (* column-index (+ choc-cap-width space-between-caps)) 0] thing))
+  (translate [(* column-index (+ choc-cap-width space-between-caps)) offset 0] thing))
+
+
+;---------------------------------------------------
+; THUMB CLUSTER
+;---------------------------------------------------
+(def thumb-offsets ;probably not parameterizing...just manually fiddling :-(
+  [{:x -38 :y -30 :rotation 65}
+   {:x -23 :y -11 :rotation 35}
+   {:x -1.5 :y -2 :rotation 10}
+   {:x 19.5 :y 0 :rotation 0}
+   {:x 41.25 :y -3 :rotation -15}
+   ])
+
+(defn translate-and-rotate-thing
+  [thing column-index offset]
+  (->> thing
+       (rotate (deg->rad (offset :rotation)) [0 0 1] )
+       (translate [(offset :x ) (offset :y) 0])
+       ))
+
+(def right-thumb 
+  (union (map-indexed (partial translate-and-rotate-thing single-hole-spaced) thumb-offsets)))
+
+(def right-thumb-translation [(* 1 (+ choc-cap-width space-between-caps))
+                              (* -1.2 (+ choc-cap-height space-between-caps))
+                              0])
 
 
 ;---------------------------------------------------
 
-(def left-fingers-homerow
+(def right-fingers-homerow
   (union (map-indexed (partial translate-thing-by-offset single-hole-spaced) column-offsets)))
 
-(def left-fingers
+(def right-fingers
   (union (map-indexed (partial translate-thing-by-offset (column-plate key-count-y)) column-offsets)))
 
-(def left-fingers-off-center
-  (translate [0 40 0 ]
-             left-fingers))
+(def right-hand
+  (union right-fingers (translate right-thumb-translation right-thumb)))
 
 (def right-fingers-off-center
-  (mirror [0 1 0] left-fingers-off-center))
+  (translate [40 0 0 ]
+             right-fingers))
+
+(def left-fingers-off-center
+  (mirror [1 0 0] right-fingers-off-center))
 
 (def joined-board
   (union (map #(rotate %2 [0 0 1] %)
-              [right-fingers-off-center  left-fingers-off-center]
-              [(/ π 6) (* -1 (/ π 6)) #_(/ π (/ 5 6))]))) ; using radians, unicode for pi is 03c0,  linux ctrl+shift+u then unicod  or windows alt code is 227
+              [left-fingers-off-center  right-fingers-off-center]
+              [(* -1 (/ π 6)) (* 1 (/ π 6))]))) ; using radians, unicode for pi is 03c0,  linux ctrl+shift+u then unicod  or windows alt code is 227
 
 (def key-space-test
   (union (map-indexed (partial translate-thing-by-offset (column-plate 2)) [0 0])))
 
 
 (spit "output/plate.scad"
-      (write-scad key-space-test))
+      (write-scad right-thumb))
       ;(write-scad joined-board))
